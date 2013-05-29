7f506cbd31e5135dadb05ca511c3dc4ef1155338
Ryan Koesters <rmkoesters@gmail.com>
2013-05-29 13:40:33 -0500

    Added cmd/cat.

---
+++ cmd/cat/cat.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	_ "github.com/rkoesters/goblin/lib"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		cat(os.Stdin)
+		return
+	}
+
+	for i := 1; i < len(os.Args); i++ {
+		f, err := os.Open(os.Args[i])
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		cat(f)
+		f.Close()
+	}
+}
+
+func cat(r io.Reader) {
+	_, err := io.Copy(os.Stdout, r)
+	if err != nil {
+		log.Print(err)
+	}
+}
---
+++ lib/log.go
@@ -0,0 +1,11 @@
+package lib
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix(os.Args[0] + ": ")
+}
