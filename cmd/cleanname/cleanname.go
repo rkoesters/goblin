3129658b7ba6eb220716365b5f3bb33ecb8056f2
Ryan Koesters <rmkoesters@gmail.com>
2013-05-29 23:27:41 -0500

    Added cmd/cleanname.

---
+++ cmd/cleanname/cleanname.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"path"
+	"flag"
+	"fmt"
+)
+
+var pwd = flag.String("d", "", "prefix")
+
+func main() {
+	flag.Parse()
+
+	for _, i := range flag.Args() {
+		if i[0:1] != "/" {
+			fmt.Println(path.Join(*pwd, i))
+		} else {
+			fmt.Println(path.Clean(i))
+		}
+	}
+}
