e76f9e8bd694b734de670cbceb387df761f44235
Ryan Koesters <rmkoesters@gmail.com>
2013-05-30 01:12:58 -0500

    Added cmd/mtime.

---
+++ cmd/mtime/mtime.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/rkoesters/goblin/lib"
+	"log"
+	"os"
+)
+
+func main() {
+	status := 0
+	for i := 1; i < len(os.Args); i++ {
+		fi, err := os.Stat(os.Args[i])
+		if err != nil {
+			log.Print(err)
+			status = 1
+		} else {
+			fmt.Printf("%11vd %v\n", fi.ModTime().Unix(), os.Args[i])
+		}
+	}
+	os.Exit(status)
+}
