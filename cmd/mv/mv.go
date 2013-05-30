8c70ad7bb3af99ed083b541aac66bbb3c51ef1f2
Ryan Koesters <rmkoesters@gmail.com>
2013-05-30 18:12:46 -0500

    Added cmd/mv.

---
+++ cmd/mv/mv.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/rkoesters/goblin/lib"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	lib.Usage = "file name | file... dir"
+	lib.InitLog()
+
+	if len(os.Args) < 3 {
+		lib.PrintUsage()
+		os.Exit(1)
+	}
+
+	files := os.Args[1 : len(os.Args)-1]
+	targ := os.Args[len(os.Args)-1]
+
+	for _, f := range files {
+		err := mv(f, targ)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func mv(p1, p2 string) error {
+	err := os.Rename(p1, p2)
+	if err == nil {
+		return nil
+	}
+
+	return os.Rename(p1, filepath.Join(p2, filepath.Base(p1)))
+}
