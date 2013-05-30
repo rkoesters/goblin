7e142199799c7e65f6345c24ed00a8083f74ce43
Ryan Koesters <rmkoesters@gmail.com>
2013-05-30 00:49:16 -0500

    Added cmd/sleep.

---
+++ cmd/sleep/sleep.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/rkoesters/goblin/lib"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	t, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(time.Duration(t) * time.Second)
+}
