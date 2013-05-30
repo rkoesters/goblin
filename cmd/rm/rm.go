577787e3da5a607a56073e45d087bacb625da7fc
Ryan Koesters <rmkoesters@gmail.com>
2013-05-30 16:48:16 -0500

    Added cmd/rm.

---
+++ cmd/rm/rm.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"github.com/rkoesters/goblin/lib"
+	"log"
+	"os"
+)
+
+var (
+	force   = flag.Bool("f", false, "do not report errors")
+	recurse = flag.Bool("r", false, "recursively remove")
+)
+
+func main() {
+	lib.Usage = "[-f] [-r] file..."
+	lib.InitFlag()
+	lib.InitLog()
+
+	flag.Parse()
+
+	var rm func(string) error
+	if *recurse {
+		rm = os.RemoveAll
+	} else {
+		rm = os.Remove
+	}
+
+	status := 0
+	for _, path := range flag.Args() {
+		err := rm(path)
+		if !*force && err != nil {
+			log.Print(err)
+			status = 1
+		}
+	}
+
+	os.Exit(status)
+}
