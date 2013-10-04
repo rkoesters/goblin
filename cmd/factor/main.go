25c5452c17a579c1c6f801c006b6a6e95bd6f8b1
Ryan Koesters <rmkoesters@gmail.com>
2013-10-03 23:32:28 -0500

    Added cmd/factor.

---
+++ cmd/factor/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/rkoesters/goblin/lib"
+	"log"
+	"math"
+	"strconv"
+)
+
+func main() {
+	lib.Usage = "[number...]"
+	lib.InitLog()
+	lib.InitFlag()
+	flag.Parse()
+
+	for _, i := range flag.Args() {
+		n, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		factors := factor(n)
+		fmt.Println(n)
+		for _, j := range factors {
+			fmt.Printf("     %v\n", j)
+		}
+		fmt.Println("")
+	}
+}
+
+func factor(n int) []int {
+	factors := make([]int, 0)
+	k := 2
+
+	for n > 1 {
+		if n%k == 0 {
+			n = n / k
+			factors = append(factors, k)
+		} else {
+			for k++; !isPrime(k); k++ {
+			}
+		}
+	}
+	return factors
+}
+
+func isPrime(num int) bool {
+	for i := 2; float64(i) <= math.Sqrt(float64(num)); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
