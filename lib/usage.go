cd161463070df2e80c55f4e7bd4fe138242875e9
Ryan Koesters <rmkoesters@gmail.com>
2013-10-07 17:08:42 -0500

    Changed the way lib/usage.go works.

--- lib/usage.go
+++ lib/usage.go
@@ -16,5 +16,21 @@ func InitFlag() {
 }
 
 func PrintUsage() {
-	fmt.Fprintf(os.Stderr, "usage:\n\t%v %v\n", argv0, Usage)
+	var format string
+	switch numFlags() {
+	case 0:
+		format = "usage: %v %v\n"
+	default:
+		format = "usage: %v [OPTIONS] %v\n"
+	}
+	fmt.Fprintf(os.Stderr, format, argv0, Usage)
+}
+
+// Returns the number of flags that have been declared.
+func numFlags() int {
+	n := 0
+	flag.VisitAll(func(f *flag.Flag) {
+		n++
+	})
+	return n
 }
